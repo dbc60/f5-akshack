e5418d432c16f912824238d54da836e163cb7ce5
Mario Arriaga <ing.mario.arriaga@gmail.com>
2022-04-01 17:14:27 +0000

    helm upgrade dns label

--- Student/Resources/Challenge 9/helm-upgrade.sh
+++ Student/Resources/Challenge 9/helm-upgrade.sh
@@ -1,2 +1,1 @@
-helm upgrade ingress-nginx ingress-nginx/ingress-nginx --set controller.service.annotations."service\│
-.beta\.kubernetes\.io/azure-dns-label-name"=kubernators --namespace $NAMESPACE
+helm upgrade ingress-nginx ingress-nginx/ingress-nginx --set controller.service.annotations."service\│.beta\.kubernetes\.io/azure-dns-label-name"=kubernators --namespace $NAMESPACE
